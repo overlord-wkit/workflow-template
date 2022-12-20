da2857bcc6864daf548398a67d3ced12d48b3665
Mikołaj Młyńczak <mikolaj.mlynczak@weknowit.nu>
2022-12-20 02:30:40 +0100

    Add failing test
    Signed-off-by: Mikołaj Młyńczak <mikolaj.mlynczak@weknowit.nu>

--- src/test/basic.test.js
+++ src/test/basic.test.js
@@ -3,5 +3,5 @@ import { expect, test } from "vitest";
 // Edit an assertion and save to see HMR in action
 
 test("Math.sqrt()", () => {
-  expect(Math.sqrt(4)).toBe(2);
+  expect(Math.sqrt(4)).toBe(1);
 });
